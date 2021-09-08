c649e96408ac0827c8e0e518bdc6154b69f1c223
VickyMastro <vicmastroianni1702@gmail.com>
2021-09-08 16:02:36 -0300

    OD-6: agregue estilos al titulo

--- src/TodoCounter.jsx
+++ src/TodoCounter.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import "TodoCounter.css";
-
-/* OPCION PARA ESTILOS EN LINEA O ESTILOS EN ETIQUETAS EN REACT*/
-/* const estilos = {
-  color: "red",
-  backgroundColor : "black",
-} */
+import "./TodoCounter.css";
 
 function TodoCounter (){
     return (
-      <h2 /* style= {estilos}  */>Completaste 2 de 3 TODOs</h2>
+      <h2 className = "todoCounter" >Completaste 2 de 3 TODOs</h2>
 
     );
 }
 
 export { TodoCounter };
+
+/* OPCION PARA ESTILOS EN LINEA O ESTILOS EN ETIQUETAS EN REACT*/
+/*   const estilos = {
+  color: "red",
+  backgroundColor : "black",
+} 
+
+<h2 style= {estilos} >Completaste 2 de 3 TODOs</h2>  */
