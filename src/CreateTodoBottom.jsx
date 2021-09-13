9b2b04aa832276e8c8f4bdd3b61608730a87cd1e
VickyMastro <vicmastroianni1702@gmail.com>
2021-09-13 20:11:52 -0300

    evento para mostrar modal para crear un TODO

--- src/CreateTodoBottom.jsx
+++ src/CreateTodoBottom.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import "./CreateTodoBottom.css";
 
 function CreateTodoBottom (){
+    const click = () => {
+      alert("Aqui se deberia abrir el modal")
+    }
     return (
-      <button className= "Button"> + </button>
+      <button className= "Button"
+        onClick = {click}
+      > 
+      + 
+      </button>
 
     );
 }
