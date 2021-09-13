0c4d3b142e3cfea5d38bd1468d520ab1c3035031
VickyMastro <vicmastroianni1702@gmail.com>
2021-09-13 20:07:33 -0300

    Paso props por param para usar el estado desde app

--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./TodoSearch.css"
 
-function TodoSearch (){
-    return (
-        <input className = "InputSearch" placeholder="Escribe tu tarea..."  /> 
-      
+function TodoSearch ({searchValue, setSearchValue}){
+    
+    const changes = (event) => {
+        setSearchValue(event.target.value);
+    }
+    
+    return ( 
+        <input 
+            className = "InputSearch" 
+            placeholder="Escribe tu tarea..."  
+            value={searchValue}
+            onChange={changes}
+        />
     );
 }
 
