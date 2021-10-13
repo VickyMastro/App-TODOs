37f7a1fc654b1a55ffad6d9a0d23b45cede8cc3e
VickyMastro <vicmastroianni1702@gmail.com>
2021-10-12 21:43:43 -0300

    Importo contexto y creo funcion para cerrar form si se toca fuera del form

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,9 +1,23 @@
 import ReactDOM from 'react-dom';
+import React from "react";
+import { TodoContext } from "../context/TodoContext";
 import "./modal.css";
 
 function Modal({children}){
+  const {setOpenModal} = React.useContext(TodoContext)
+
+  const click = (event) => {
+    if (event.target.id === "modalBackground") {
+      setOpenModal(false)
+    }
+  }
+
     return ReactDOM.createPortal(
-      <div className= "modalBackground">
+      <div 
+        className= "modalBackground" 
+        id="modalBackground" 
+        onClick={click}
+        >
         {children}
       </div>,
         document.getElementById("modal")
